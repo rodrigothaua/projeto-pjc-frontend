cbcfeb932d7f8aa3187a54f10d89798fc0fd903d
Rodrigo Thauã <rodrigothaua@gmail.com>
2025-03-28 10:24:30 -0400

    Inclusão de tratamento de erro para exibir mensagem caso a chamada à API falhar

--- src/components/ListaDesaparecidos.js
+++ src/components/ListaDesaparecidos.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { listarDesaparecidos } from '../services/api';
 
 function ListaDesaparecidos() {
+  const [desaparecidos, setDesaparecidos] = useState([]);
+  const [erro, setErro] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+            const data = await listarDesaparecidos();
+            if (data && Array.isArray(data)) {
+            setDesaparecidos(data);
+            } else {
+            setErro('Erro ao buscar dados dos desaparecidos.');
+            }
+        } catch (error) {
+        setErro('Erro ao buscar desaparecidos: ' + error.message);
+        }
+    }
+    fetchData();
+  }, []);
+
+  if (erro) {
+    return <div>{erro}</div>;
+  }
+
   return (
     <div>
-      {/* Implementar a lógica para listar os desaparecidos */}
+      <h2>Pessoas Desaparecidas</h2>
+      <ul>
+        {desaparecidos.map((desaparecido) => (
+          <li key={desaparecido.id}>
+            {desaparecido.nome} - <Link to={`/detalhe/${desaparecido.id}`}>Ver Detalhes</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
