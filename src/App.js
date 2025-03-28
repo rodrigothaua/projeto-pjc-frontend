d1f1801044ac628a518ec77868e58f34a901da12
Rodrigo Thauã <rodrigothaua@gmail.com>
2025-03-28 09:17:58 -0400

    Implementa reteamento e Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ListaDesaparecidos from './components/ListaDesaparecidos';
+import DetalheDesaparecido from './pages/DetalheDesaparecido';
+import FormularioInformacoes from './pages/FormularioInformacoes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ListaDesaparecidos />} />
+        <Route path="/detalhe/:id" element={<DetalheDesaparecido />} />
+        <Route path="/informacoes/:id" element={<FormularioInformacoes />} />
+      </Routes>
+    </Router>
   );
 }
 
